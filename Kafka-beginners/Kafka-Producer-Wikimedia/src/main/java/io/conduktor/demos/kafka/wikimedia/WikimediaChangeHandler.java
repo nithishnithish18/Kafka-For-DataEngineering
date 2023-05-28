dbd4a4c3eb5e0ca5bd37d79d8fa15d26be4b56f3
nitheesh <nithishprabakaran09@gmail.com>
2023-05-28 18:45:02 +0530

    OpenSearch Consumer Implementation

---
+++ Kafka-beginners/Kafka-Producer-Wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,48 @@
+package io.conduktor.demos.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import com.sun.jdi.event.StepEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements EventHandler {
+
+    KafkaProducer<String, String> kafkaProducer;
+    String topic;
+    private final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getSimpleName());
+
+    public WikimediaChangeHandler(KafkaProducer<String,String> kafkaProducer,String topic){
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() throws Exception {
+        //do nothing
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+        kafkaProducer.close();
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) throws Exception {
+        log.info(messageEvent.getData());
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String comment) throws Exception {
+        //do nothing
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        log.error("Error in stream reading", t);
+    }
+}
